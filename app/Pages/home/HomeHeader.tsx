ba97caa0d52d2a136236b0a66649b4970bf6076b
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2024-10-10 17:09:19 +0200

    tel dans <a>

--- app/Pages/home/HomeHeader.tsx
+++ app/Pages/home/HomeHeader.tsx
@@ -14,7 +14,9 @@ export default function HomeHeader() {
           loading="eager"
         />
       </h1>
-      <p className="header_title_description">06 90 01 02 03</p>
+      <a href="tel:0690010203" className="header_title_description">
+        06 90 01 02 03
+      </a>
       <div className="header_title_link">
         <Link href={"/menu"}>Découvrir notre menu</Link>
         <Link href={"/#location"}>Localisation</Link>
